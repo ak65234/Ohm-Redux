8ad61e5c05e5b7bac99357e21d0c96f161dd48b8
ak65234 <6294860+ak65234@users.noreply.github.com>
2018-03-10 04:49:56 -0600

    Initialized all hardware in separate class

---
+++ src/org/usfirst/frc/team1817/robot/Hardware.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team1817.robot;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Hardware {
+    private final double DISTANCE_PER_PULSE = Math.PI * 6.0 / 250.0;
+
+    public final DifferentialDrive chassis;
+    public final Encoder leftEncoder, rightEncoder;
+    public final ADXRS450_Gyro gyro;
+    public final PowerDistributionPanel pdp;
+    public final Servo frontShifter, backShifter;
+    public final SpeedControllerGroup intake;
+    public final VictorSP wrist;
+
+    public Hardware(){
+        VictorSP left = new VictorSP(1);
+        VictorSP right = new VictorSP(0);
+        chassis = new DifferentialDrive(left, right);
+
+        VictorSP intake1 = new VictorSP(2);
+        intake1.setInverted(true);
+        VictorSP intake2 = new VictorSP(6);
+        intake = new SpeedControllerGroup(intake2, intake1);
+
+        wrist = new VictorSP(7);
+
+        frontShifter = new Servo(8);
+        backShifter = new Servo(9);
+
+        leftEncoder = new Encoder(0, 1);
+        leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        leftEncoder.setReverseDirection(true);
+
+        rightEncoder = new Encoder(2, 3);
+        rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+
+        pdp = new PowerDistributionPanel();
+
+        gyro = new ADXRS450_Gyro();
+        gyro.calibrate();
+    }
+}
