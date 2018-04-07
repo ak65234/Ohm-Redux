47b6ed0c6cc3ad66b62b3ecb177fa667188249f5
BrianMichell <ecwhodie@yahoo.com>
2018-04-06 20:50:40 -0500

    Commit of work done on 4/3/2018

--- src/org/usfirst/frc/team1817/robot/Sensor_Watcher.java
+++ src/org/usfirst/frc/team1817/robot/Sensor_Watcher.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1817.robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Sensor_Watcher implements Runnable{
@@ -19,6 +20,7 @@ public class Sensor_Watcher implements Runnable{
 			SmartDashboard.putNumber("Right encoder", hw.rightEncoder.getDistance());
 			SmartDashboard.putNumber("Gyro", hw.gyro.getAngle());
 			SmartDashboard.putNumber("Amperage Draw", hw.pdp.getTotalCurrent());
+			Timer.delay(0.005);
 		}
 		
 	}
