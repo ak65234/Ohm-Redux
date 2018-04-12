8ba8ff7af6d33464547460e32315758b62b92a62
ak65234 <6294860+ak65234@users.noreply.github.com>
2018-04-12 00:24:17 -0500

    Added POV constants

--- src/org/usfirst/frc/team1817/robot/Controls.java
+++ src/org/usfirst/frc/team1817/robot/Controls.java
@@ -5,6 +5,13 @@ import edu.wpi.first.wpilibj.XboxController;
 public class Controls {
     public final XboxController driver, manipulator;
 
+    public static class POV {
+        public static final int UP = 0;
+        public static final int DOWN = 180;
+        public static final int LEFT = 90;
+        public static final int RIGHT = 270;
+    }
+
     public Controls() {
         driver = new XboxController(0);
         manipulator = new XboxController(1);
