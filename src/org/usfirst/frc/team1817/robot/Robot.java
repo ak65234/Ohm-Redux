036ccb892b9939cf734e7fa2ed44da46fe9ffd30
BrianMichell <ecwhodie@yahoo.com>
2018-04-12 21:20:47 -0500

    Added rumble feedback for cube grabbing
    Need to only apply this when the hand is extended. Also need to clean it up.

--- src/org/usfirst/frc/team1817/robot/Robot.java
+++ src/org/usfirst/frc/team1817/robot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1817.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID;
 
@@ -102,6 +103,17 @@ public class Robot extends TimedRobot {
 		}
 
 		fingers.setSpeed(dRT - dLT);
+		
+		if(hw.cubeSensor.getVoltage()>1.05) {
+			ctrls.driver.setRumble(RumbleType.kLeftRumble, 1);
+		} else {
+			ctrls.driver.setRumble(RumbleType.kLeftRumble, 0);
+		}
+		if(hw.cubeSensor.getVoltage()>1.2) {
+			ctrls.driver.setRumble(RumbleType.kRightRumble, 1);
+		} else {
+			ctrls.driver.setRumble(RumbleType.kRightRumble, 0);
+		}
 	}
 
 	@Override
