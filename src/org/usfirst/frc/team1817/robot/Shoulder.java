dd0a00de7ea0ebc120b37a7e4b7cc29cb93f2005
BrianMichell <ecwhodie@yahoo.com>
2018-04-19 08:05:12 -0500

    Added manual control

--- src/org/usfirst/frc/team1817/robot/Shoulder.java
+++ src/org/usfirst/frc/team1817/robot/Shoulder.java
@@ -8,13 +8,34 @@ public class Shoulder implements Runnable{
 	SpeedControllerGroup shoulder;
 	Encoder shoulderEncoder;
 	
+	Thread t;
+	
+	private final double DEADBAND = 0.05;
+	
 	public Shoulder(Hardware hw) {
 		this.shoulder = hw.shoulder;
 		this.shoulderEncoder = hw.shoulderEncoder;
+		t = new Thread(this, "Shoulder");
+		t.start();
+		
 	}
 	
 	public void run() {
+		/*
+		while(!Thread.interrupted()) {
 		
+		}
+		*/
+	}
+	
+	private boolean validRange(double value) {
+		return Math.abs(value) > DEADBAND;
+	}
+	
+	public void manualMove(double value) {
+		if (validRange(value)) {
+			shoulder.set(value);
+		}
 	}
 	
 }
